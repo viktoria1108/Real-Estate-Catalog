94199dcbdedd105b1b47bd2dadbe140e0fcaef00
Krymchuc <khrystyna.krymchuk@oa.edu.ua>
2022-10-18 18:41:25 +0300

    Accommodation(model and controller)

---
+++ Real-State-Catalog/Real-State-Catalog/Controllers/AccommodationController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Real_State_Catalog.Models;
+
+namespace Real_State_Catalog.Controllers
+{
+    public class AccommodationController : Controller
+    {
+
+    }
+}
---
+++ Real-State-Catalog/Real-State-Catalog/Models/Accommodation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Real_State_Catalog.Models
+{
+    public class Accommodation
+    {
+        public Guid Id { get; set; }
+        public string UserId { get; set; }
+        [Required(ErrorMessage = "You must enter a name for your accommodation")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "You must select the housing type")]
+        [Display(Name = "Type")]
+        [RegularExpression("Apartment|House|Room in apartment|Room in house", ErrorMessage = "Please select a valid slot type")]
+        public string Type { get; set; }
+        [Required(ErrorMessage = "You must specify the maximum number of travelers")]
+        [Display(Name = "Maximum Travelers")]
+        public int MaxTraveler { get; set; }
+        [Required(ErrorMessage = "You must enter the description of your accommodation")]
+        public string Description { get; set; }
+    }
+}
+        
