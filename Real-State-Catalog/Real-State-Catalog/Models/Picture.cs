26b0678b090b2a397e9fc0ab7f65a3ccb6c523f6
Krymchuc <khrystyna.krymchuk@oa.edu.ua>
2022-10-18 20:11:56 +0300

    Booking(model and controller)

---
+++ Real-State-Catalog/Real-State-Catalog/Models/Picture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Real_State_Catalog.Models
+{
+    public class Picture
+    {
+        public Guid Id { get; set; }
+
+        public Guid AccommodationId { get; set; }
+
+        public string FileName { get; set; }
+
+        public Picture(Guid accommodationId, string fileName)
+        {
+            this.AccommodationId = accommodationId;
+            this.FileName = fileName;
+        }
+    }
+}
